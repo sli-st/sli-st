0e41bf6b77f9f9e32a94d28d017001239e24e9a1
Simon <24327053+simontypes@users.noreply.github.com>
2021-10-21 05:59:46 -0500

    footer

--- src/components/datesnav.js
+++ src/components/datesnav.js
@@ -43,16 +43,17 @@ export default function DatesNav() {
       `}
       render={data => (
         <nav aria-label="event dates navigation" className="dates-nav sticky-top pt-4 pb-3 bg-black text-white container-fluid">
-          <div className="row justify-content-center ">
-            <div className="col-md-5 d-flex flex-row align-items-center overflow-hidden">
+          <div className="row">
+            <div className="col-md-6 d-flex overflow-hidden">
               <Link
                 to="/"
-                className="text-white px-3"
+                className="text-white"
                 activeClassName="nav-active"
               >
                 <FontAwesomeIcon icon={faBookmark} />
               </Link>
-              <div className="d-flex justify-content-between scrolling-wrapper align-items-center" >
+              <div className="px-2 text-black">|</div>
+              <div className="scrolling-wrapper align-items-center" >
                 {data.currentmonth.edges.map(({ node }) => (
                   <Link
                     to={"/" + currentMonthNum + "/" + node.name}
