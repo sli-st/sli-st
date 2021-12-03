7939f80a8234a00c1c3736408c519a8578e562b8
Simon <24327053+simontypes@users.noreply.github.com>
2021-12-02 18:48:15 -0600

    flyers

--- src/pages/s/index.js
+++ src/pages/s/index.js
@@ -10,10 +10,15 @@ const Me = () => (
     <Seo title="S" />
     <div className="container-fluid">
       <div className="row justify-content-center">
-        <div className="col-8 col-md-5">
-          <h1 className="text-center pt-5 text-secondary">[S]</h1>
-          <p className="text-center text-light">SOON!</p>
-          <nav className="d-flex flex-column pb-5 my-5 mx-auto">
+        <div className="col-8 col-md-3 d-flex flex-column">
+          <div className="text-center neon-button my-5">SLi.ST</div>
+        </div>
+        
+      </div>
+      <div className="row justify-content-center mt-5">
+        <div className="col-8 col-md-5 d-flex flex-column">
+          
+          <nav className="d-flex flex-column pb-5 my-5">
             <SNavLink to="/s/playlists/">playlists</SNavLink>
             <SNavLink to="/visualmedia" className="text-center text-decoration-none pt-3 lead mb-5 border border-primary border-bottom-0 text-dark">visual media WIP</SNavLink>
             <SNavLink to="/s/" className="text-center text-decoration-none pt-3 lead mb-5 border border-primary border-bottom-0 text-dark">recorded WIP</SNavLink>
@@ -22,6 +27,7 @@ const Me = () => (
             <SNavLink to="/s/" className="text-center text-decoration-none pt-3 lead mb-5 border border-primary border-bottom-0 text-dark">special thanks WIP</SNavLink>
           </nav>
         </div>
+        
       </div>
     </div>
   </Layout>
