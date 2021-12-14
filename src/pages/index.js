c89fe1d2f72ba3b8859a9af0dd05bfa41d720a7c
Simon <24327053+simontypes@users.noreply.github.com>
2021-12-14 02:58:47 -0600

    back

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,22 +10,22 @@ const IndexPage = () => (
   <Layout pageTitle="Index">
     <Seo title="Index" />
 
-    <div className="container-fluid">
-      <section className="row pb-5 justify-content-center">
-        <Link to="/x/" className="col-6 col-md">
+    <div className="container-fluid index">
+      <section className="row pb-5">
+        <Link to="/x/" className="col">
           <img src={"/sflyer1.jpeg"} className="img-fluid" alt="QR Code" />
         </Link>
-        <Link to="/news/" className="col-6 col-md">
+        <Link to="/news/" className="col">
           <img src={"/sflyer6.jpg"} className="img-fluid" alt="QR Code" />
         </Link>
-        <Link to="/about/" className="col-6 col-md">
+        <Link to="/about/" className="col">
           <img src={"/sflyer4.jpeg"} className="img-fluid" alt="QR Code" />
         </Link>
 
-        <a href="https://www.instagram.com/slistxmas/" className="col-6 col-md">
+        <a href="https://www.instagram.com/slistxmas/" className="col">
           <img src={"/sflyer3.jpeg"} className="img-fluid" alt="QR Code" />
         </a>
-        <Link to="/m/" className="col-6 col-md">
+        <Link to="/m/" className="col">
           <img src={"/sflyer7.jpeg"} className="img-fluid" alt="QR Code" />
         </Link>
       </section>
