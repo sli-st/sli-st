9ee2fbced2d2e7a79934e36199764793ea5c663e
Simon <24327053+simontypes@users.noreply.github.com>
2021-12-15 17:46:03 -0600

    rearranged

--- src/components/indexnav.js
+++ src/components/indexnav.js
@@ -25,16 +25,24 @@ const IndexNav = () => {
           </section>
           <section className="row justify-content-center py-4">
             <div className="col-4">
-              <div className="row">
-                <Link to="/news/" className="col-12 bbg-primary">
-                  <StaticImage src="../images/index/news.png" alt="A kitten" />
-                </Link>
-                <Link
-                  to="/sflyer3.jpeg"
-                  className="col-12 display-1 text-decoration-none text-center mt-5 text-primary "
-                >
-                  {"<3"}
-                </Link>
+              <div className="row d-flex justify-content-between flex-column">
+                <div className="col-12 pb-4">
+                  <Link to="/mixes/" className="">
+                    <StaticImage
+                      src="../images/index/recorded.png"
+                      alt="A kitten"
+                      className=""
+                    />
+                  </Link>
+                </div>
+                <div className="col-12">
+                  <Link to="/news/" className="bg-primary">
+                    <StaticImage
+                      src="../images/index/news.png"
+                      alt="A kitten"
+                    />
+                  </Link>
+                </div>
               </div>
             </div>
             <Link to="#" className="col-8 grayscale">
@@ -43,38 +51,8 @@ const IndexNav = () => {
           </section>
 
           <section className="row justify-content-center">
-            <a href="https://www.instagram.com/slistxmas/" className="col">
-              <StaticImage
-                src="../images/index/igqr.jpg"
-                alt="A kitten"
-                className=""
-              />
-            </a>
-            <Link to="/mixes/" className="col">
-              <StaticImage
-                src="../images/index/recorded.png"
-                alt="A kitten"
-                className="border border-primary"
-              />
-            </Link>
-
             <Link to="/x/" className="col">
               <StaticImage src="../images/index/x.png" alt="A kitten" />
-            </Link>
-          </section>
-
-          <section className="row justify-content-center py-4">
-            <Link
-              to="https://us5.list-manage.com/survey?u=ffa00e0e7d456f08048753fe5&id=d2d82c8053&attribution=false"
-              className="col text-cente d-flex align-items-center text-decoration-none"
-            >
-              <span className="w-100 h-100 d-flex align-items-center justify-content-center border border-light">
-                <div className="text-primary px-3">Anonymous Feedback Form</div>
-              </span>
-            </Link>
-
-            <Link to="/m/" className="col">
-              <StaticImage src="../images/index/m.jpeg" alt="A kitten" />
             </Link>
             <Link to="/m/" className="col text-decoration-none ">
               <span className="w-100 h-100 d-flex align-items-center justify-content-center">
@@ -83,6 +61,35 @@ const IndexNav = () => {
                 </div>
               </span>
             </Link>
+            <Link
+              to="#"
+              className="col text-decoration-none text-center text-primary"
+            >
+              <StaticImage src="../images/index/fire.png" alt="A kitten" />
+            </Link>
+          </section>
+
+          <section className="row justify-content-center py-4">
+            <Link
+              to="https://us5.list-manage.com/survey?u=ffa00e0e7d456f08048753fe5&id=d2d82c8053&attribution=false"
+              className="col text-cente d-flex align-items-center text-decoration-none"
+            >
+              <span className="w-100 h-100 d-flex align-items-center justify-content-center border\ border-light border-start-0">
+                <div className="text-primary text-center">
+                  Anonymous Feedback <br></br>Form
+                </div>
+              </span>
+            </Link>
+            <Link to="/#/" className="col">
+              <StaticImage src="../images/index/m.jpeg" alt="A kitten" />
+            </Link>
+            <a href="https://www.instagram.com/slistxmas/" className="col">
+              <StaticImage
+                src="../images/index/igqr.jpg"
+                alt="A kitten"
+                className=""
+              />
+            </a>
           </section>
         </div>
       </div>
