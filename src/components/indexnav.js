7c50e8aba6b619a3124ca9233355d1741c4b5e8b
Simon <24327053+simontypes@users.noreply.github.com>
2021-12-15 04:04:01 -0600

    up

--- src/components/indexnav.js
+++ src/components/indexnav.js
@@ -12,57 +12,60 @@ const IndexNav = () => {
       className="p-0 fixed-bottom"
     >
       <div className="row justify-content-center">
-        <div className="col-md-4 p-0">
+        <div className="col-md-6 col-lg-4 p-0">
           <section className="row justify-content-center">
-            <div className="col-md-4 p-0">
-              <div className="d-flex flex-column">
-                <Link to="/news/" className="col">
+            <div className="col-4 p-0">
+              <div className="row">
+                <Link to="/news/" className="col-12">
                   <StaticImage
                     src="../images/index/canary.jpg"
                     alt="A kitten"
                   />
                 </Link>
-                <Link to="/x/" className="col">
-                  <StaticImage
-                    src="../images/index/igqr.jpg"
-                    alt="A kitten"
-                    className=""
-                  />
+                <Link
+                  to="/sflyer3.jpeg"
+                  className="col-12 display-1 text-decoration-none text-center mt-4 "
+                >
+                  ❤️
                 </Link>
               </div>
             </div>
-            <Link to="/sflyer1.jpeg/" className="col-md-8">
-              <StaticImage src="../images/index/sflyer1.jpeg" alt="A kitten" />
+            <Link to="/sflyer1.jpeg/" className="col-8">
+              <StaticImage
+                src="../images/index/sflyer1.jpeg"
+                alt="A kitten"
+                className="grayscale"
+              />
             </Link>
           </section>
 
           <section className="row justify-content-center">
-            <Link
-              to="/sflyer3.jpeg"
-              className="col display-1 text-decoration-none d-flex justify-content-center align-items-center border-primary"
-            >
-              ❤️
-            </Link>
             <Link to="/x/" className="col">
-              <StaticImage src="../images/index/x.png" alt="A kitten" />
+              <StaticImage
+                src="../images/index/igqr.jpg"
+                alt="A kitten"
+                className=""
+              />
             </Link>
+            <Link to="/recorded/" className="col"></Link>
+
             <Link to="/m/" className="col">
               <StaticImage src="../images/index/m.jpeg" alt="A kitten" />
             </Link>
           </section>
 
           <section className="row justify-content-center">
-            <Link to="/about/" className="col">
-              <StaticImage src="../images/index/about.jpeg" alt="A kitten" />
-            </Link>
-            <Link to="/recorded/" className="col">
-              <StaticImage src="../images/index/recorded.png" alt="A kitten" />
-            </Link>
             <Link
               to="/sflyer3.jpeg"
-              className="col display-1 text-decoration-none d-flex justify-content-center align-items-center border-primary"
+              className="col display-5 text-decoration-none d-flex align-items-center text-primary"
             >
-              ❤️
+              s@sli.st
+            </Link>
+            <Link to="/x/" className="col">
+              <StaticImage src="../images/index/x.png" alt="A kitten" />
+            </Link>
+            <Link to="/about/" className="col">
+              <StaticImage src="../images/index/about.jpeg" alt="A kitten" />
             </Link>
           </section>
         </div>
