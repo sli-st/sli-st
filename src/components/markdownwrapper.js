db14dded6bffa230f06727c05d2bda103fd10e05
Simon <24327053+simontypes@users.noreply.github.com>
2021-12-31 12:23:46 -0600

    changes

--- src/components/markdownwrapper.js
+++ src/components/markdownwrapper.js
@@ -4,7 +4,7 @@ const MarkdownWrapper = ({ children }) => {
   return (
     <article
       className="pt-5 mx-auto prose prose-stone dark:prose-invert prose-headings:font-serif 
-    prose-p:font-serif prose-p:leading-loose  lg:prose-lg lg:leading-8 prose-headings:font-normal  "
+    prose-p:font-serif prose-p:leading-loose  lg:prose-lg lg:leading-8 prose-headings:font-normal prose-headings:leading-tight "
     >
       {children}
     </article>
