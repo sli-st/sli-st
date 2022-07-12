deab36bb3a385dcfec6efa1d6c94dc0f0e7379a2
Simon <24327053+simontypes@users.noreply.github.com>
2022-07-12 00:36:54 -0500

    Update markdownwrapper.js

--- src/components/markdownwrapper.js
+++ src/components/markdownwrapper.js
@@ -3,8 +3,8 @@ import * as React from "react"
 const MarkdownWrapper = ({ children }) => {
   return (
     <article
-      className="pt-5 mx-auto prose prose-stone dark:prose-invert prose-headings:font-serif 
-    prose-p:font-serif prose-p:leading-loose  lg:prose-lg lg:leading-8 prose-headings:font-normal prose-headings:leading-tight "
+      className="pt-5 mx-auto prose text-stone-300 prose-headings:font-serif prose-headings:text-white 
+    prose-p:font-serif prose-p:leading-loose lg:prose-lg lg:leading-8 prose-headings:font-normal prose-headings:leading-tight "
     >
       {children}
     </article>
