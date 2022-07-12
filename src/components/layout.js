73ffa6d813139960842707507639377cc6be3e0e
Simon <24327053+simontypes@users.noreply.github.com>
2022-07-12 04:35:06 -0500

    up

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,8 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import Header from "../components/header"
 import { Script } from "gatsby"
-import { Partytown } from "@builder.io/partytown/react"
 
 import "./styles.css"
-// import "./layout.scss"
 
 const Layout = ({ pageTitle, children, to, label }) => {
   return (
@@ -15,20 +13,10 @@ const Layout = ({ pageTitle, children, to, label }) => {
       <main className="pt-5">
         <div className="container">{children}</div>
       </main>
-
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=G-80661C8JJ3`}
-        strategy="off-main-thread"
-        forward={[`gtag`]}
+        src="https://www.googletagmanager.com/gtag/js?id=G-80661C8JJ3"
+        strategy="post-hydrate"
       />
-      <Script id="gtag-config" strategy="off-main-thread">
-        {`
-    window.dataLayer = window.dataLayer || []
-    window.gtag = function gtag() { window.dataLayer.push(arguments) }
-    gtag('js', new Date())
-    gtag('config', G-80661C8JJ3, { send_page_view: false })
-  `}
-      </Script>
     </div>
   )
 }
