da83dfa50bc71ede6f0f41023b27bf1e48ce741d
Simon <24327053+simontypes@users.noreply.github.com>
2022-07-23 18:15:43 -0500

    Update epk.js

--- src/pages/epk.js
+++ src/pages/epk.js
@@ -25,26 +25,30 @@ const BlogPage = ({ data }) => {
           <br />
           sli.st is 25 and from NYC, but despite all that he had never been to a
           techno rave until March 2021. 3 months later, he came to CDMX on a
-          dare to find a supposedly nonexistent underground scene. Within his
-          first weekend, he was completely addicted to the darker sound of CDMX
-          and immediately cancelled all of his travel plans. By the end of
+          dare to search for a supposedly nonexistent underground scene. Within
+          his first weekend, he was completely addicted to the darker sound of
+          CDMX and immediately cancelled all future travel plans. By the end of
           August he was learning how to DJ from one of CDMX's nastiest
-          industrial DJs. Within the following month, he was invited to DJ at
-          The Real Under a couple of times, open for B Space at 49 Basan, and
-          also DJ for a few hours at the New Year's after party at NIX.
+          industrial DJs. Within the following month, he was invited to play at
+          The Real Under a couple times, open for B Space at 49 Basan, and DJ
+          for a few hours at the New Year's after party at NIX.
         </p>
         <br />
         <p>
           <span className="font-bold text-primary">Genres:</span> <br></br>
           dark ambient, dark melodic house and techno, minimal, apocalyptic
-          industrial techno, gabber, dnb and other genres but with a darker
-          twist.
+          industrial techno, gabber, dnb, and other genres- but almost all with
+          a dark twist.
         </p>
         <p>
-          sli.st is going through a darkwave and trancey phase. His sets tend to
-          include bits of unnexpected genres and cultural shoutouts as a
-          reminder that most people have multiple layers: dembow, synthwave,
-          psytechno, rock, etc. etc.
+          sli.st is currently going through a minimal and indusrial psy phase.
+          Nevertheless, his sets are dynamic with small shoutouts to unnexpected
+          genres such as synthwave, dembow, and even rock. He likes to weave in
+          seemingly random genres to earn his audience's trust before taking
+          them on a deep dive into some truly dark angry scary cathartic
+          melodies. His sets are designed to serve as a reminder that you and
+          your fellow ravers in the underground scene often have multiple layers
+          underneath all your black clothing.
         </p>
         <br></br>
         <p>
@@ -82,8 +86,8 @@ const BlogPage = ({ data }) => {
           <br></br>
           <ul>
             <li>
-              "sli.st" should be kept lowercase as it is a reference to the URL
-              of this website
+              "sli.st" should preferably be kept lowercase as it is a reference
+              to the URL of this website
             </li>
             <li>
               if the font can be specified, please use Arial, or another
